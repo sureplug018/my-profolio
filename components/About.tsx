af2f7633745b9442b067406234af71789f06d6f6
sureplug018 <ezea5149@gmail.com>
2025-10-17 15:43:41 +0100

    commit

--- components/About.tsx
+++ components/About.tsx
@@ -7,18 +7,18 @@ function About() {
       <div className='w-[90%] text-center m-auto lg:w-[70%]'>
         <Title text='About Me' />
         <p className='mb-4'>
-          I'm Raphael Eze, a full-stack developer passionate about crafting
+          I&apos;m Raphael Eze, a full-stack developer passionate about crafting
           innovative, user-focused web applications. With a knack for
           problem-solving and a commitment to clean, efficient code, I build
           dynamic, responsive web applications that blend creativity and
           functionality. Connect with me to collaborate on exciting projects.
         </p>
         <p>
-          Outside of coding, I’m an avid gamer, diving into immersive video game
-          worlds that spark creativity and sharpen my strategic thinking. I’m
-          equally excited about learning the latest tech innovations, from AI
-          advancements to cutting-edge frameworks, keeping me inspired and ahead
-          of the curve in the ever-evolving tech landscape.
+          Outside of coding, I&apos;m an avid gamer, diving into immersive video
+          game worlds that spark creativity and sharpen my strategic thinking.
+          I&apos;m equally excited about learning the latest tech innovations,
+          from AI advancements to cutting-edge frameworks, keeping me inspired
+          and ahead of the curve in the ever-evolving tech landscape.
         </p>
       </div>
     </Container>
